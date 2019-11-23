f405c7cf282e9931e56da5400cb01c8aa108ae8a
Stephanie SanFilippo <mavnyin88@gmail.com>
2019-11-23 17:04:59 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+function playAudio(e) {
+  console.log(e);
+  if (e.type === "click") {
+    audio = document.querySelector('audio[data-key="13"]');
+  } else if (e.type === "keydown") {
+    audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  }
+  // const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  if (!audio) {
+    return;
+  }
+  var transition = document.querySelector(".button");
+  transition.classList.add("active");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== "transform") return;
+  event.target.classList.remove("active");
+}
+
+var transition = document.querySelector(".button");
+transition.addEventListener("transitionend", removeTransition);
+
+window.addEventListener("keydown", playAudio, false);
+document.querySelector(".button").addEventListener("click", playAudio, false);
